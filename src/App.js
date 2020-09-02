fb40f4f7c36fe55bd2c39aff996fa09f14929dd5
adarleika <adarleika@gmail.com>
2020-09-02 15:03:51 -0400

    agregar logo y contenedor en pagina principal

--- src/App.js
+++ src/App.js
@@ -7,22 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+ 
         <section className="inicio">
 
         <InicioLogin/>
         </section>
-        <p>
-          Hola a todos <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
--- src/components/InicioLogin.js
+++ src/components/InicioLogin.js
@@ -1,12 +1,15 @@
 import React from "react";
+import logo_size from '../assets/imagenes/logo_size.jpg'; 
 
 class InicioLogin extends React.Component{
      render(){
         return (
             <React.Fragment>
+                <div className="contenedor"> 
+                <img src={logo_size} className="App-logo" alt="logo_size"/> 
                 <h1> hello soy el componente de InicioLogin </h1>
                 
-
+                </div>
             </React.Fragment>
             
         );
