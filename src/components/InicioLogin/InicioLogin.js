adcdf79ad9b9fa64babe4dd60f0de32a717af217
adarleika <adarleika@gmail.com>
2020-09-03 20:59:17 -0400

    funciona el boton img mesero cambio de pantalla - arreglo css menu lateral

--- src/components/InicioLogin/InicioLogin.js
+++ src/components/InicioLogin/InicioLogin.js
@@ -3,6 +3,10 @@ import logo_size from '../../assets/imagenes/logo_size.jpg';
 import camarero from '../../assets/imagenes/camarero.png';
 import cocinero from '../../assets/imagenes/cocinero.png';
 import './InicioLogin.css';
+import PedidosMesero from '../../view/viewMasterMesero';
+import { NavLink } from 'react-router-dom';
+
+
 
 class InicioLogin extends React.Component{
      render(){
@@ -11,14 +15,31 @@ class InicioLogin extends React.Component{
                 <div className="contenedor"> 
                 <img src={logo_size} className="App-logo" alt="logo_size"/> 
                 
-                <img src={camarero} className="camarero" alt="camarero"/> 
+                <img src={camarero} className="camarero" alt="camarero"/>
                 <img src={cocinero} className="cocinero" alt="cocinero"/> 
+
+                <NavLink to="/viewMasterMesero" src={camarero} className="camarero" alt="camarero"></NavLink>
+
                 </div>
+                
             </React.Fragment>
             
+            
         );
+     
+      }
 
+
+     myfunction(){
+        //const PedidosMesero = useState([])
+        return (
+            //PedidosMesero
+            <PedidosMesero/>
+   
+        );
+ 
      }
+     
+    }
 
-}
 export default InicioLogin;
--- src/components/MenuLateral/MenuLateral.js
+++ src/components/MenuLateral/MenuLateral.js
@@ -6,14 +6,13 @@ class MenuLateral extends React.Component{
     render () {
         return(
             <React.Fragment>
-           <h1> esto es la vista del menu lateral</h1>
-           <div className="lateralmenu">
-            <img src={camarero} className="camarero" alt="camarero"/> 
-            <button id="pedido" type="button" class="button">HACER PEDIDO</button>
-            <button id="mipedido" type="button" class="button">MIS PEDIDO</button>
-        </div>
-    
-    </React.Fragment>
+            <h1> esto es la vista del menu lateral</h1>
+                <div className="lateralmenu">
+                    <img src={camarero} className="camarero" alt="camarero"/> 
+                </div>
+                <button id="pedido" type="button" class="button1">HACER PEDIDO</button>
+                <button id="mipedido" type="button" class="button2">MIS PEDIDO</button>
+            </React.Fragment>
         );
    }
     
