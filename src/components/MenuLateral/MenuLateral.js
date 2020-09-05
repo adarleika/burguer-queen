38b02b6a53c4c8f0d3438907c6029831aa2cf2ca
adarleika <adarleika@gmail.com>
2020-09-04 23:08:23 -0400

    css componente menulateral

--- src/components/MenuLateral/MenuLateral.js
+++ src/components/MenuLateral/MenuLateral.js
@@ -6,12 +6,13 @@ class MenuLateral extends React.Component{
     render () {
         return(
             <React.Fragment>
-            <h1> esto es la vista del menu lateral</h1>
+            {/*<h1> esto es la vista del menu lateral</h1>*/}
                 <div className="lateralmenu">
-                    <img src={camarero} className="camarero" alt="camarero"/> 
-                </div>
+                    <img src={camarero} class="img-izq" alt="camarero"/> 
+               
                 <button id="pedido" type="button" class="button1">HACER PEDIDO</button>
                 <button id="mipedido" type="button" class="button2">MIS PEDIDO</button>
+                 </div>
             </React.Fragment>
         );
    }
