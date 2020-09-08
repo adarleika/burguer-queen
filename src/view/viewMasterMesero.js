95c4de21e57b9604775977021d281412efc9c822
marceortega <marcela.ortegao@mayor.cl>
2020-09-08 10:06:03 -0300

    componente ensalada,json ensalada,bebidas,postres

--- src/view/viewMasterMesero.js
+++ src/view/viewMasterMesero.js
@@ -3,11 +3,13 @@ import MenuCarta from '../components/MenuCarta/MenuCarta'
 import MenuLateral from '../components/MenuLateral/MenuLateral'
 import MenuPedido from '../components/MenuPedido/MenuPedido'
 import MenuAlmuerzo from '../components/MenuAlmuerzo/MenuAlmuerzo'
+import MenuEnsaladas from '../components/MenuEnsaladas/MenuEnsaladas'
 
 
 function PedidosMesero() {
     return(
     <div>
+        <MenuEnsaladas/>
         <MenuAlmuerzo/>
         <MenuCarta/>
         <MenuLateral/>
