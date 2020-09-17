83e5bdaa36e3e317ed011bcb61e0ad3492937a72
marceortega <marcela.ortegao@mayor.cl>
2020-09-16 21:03:43 -0300

    agregarFirebase coleccion Pedidos

--- src/components/MenuEnsaladas/Ensaladas.js
+++ src/components/MenuEnsaladas/Ensaladas.js
@@ -1,7 +1,10 @@
 import React from 'react'
+//import firebase from 'firebase/app'
+import 'firebase/firestore'
+import {firebase} from '../../firebase'
 
 
-const Ensaladas = ({ ensaladita, cart, setCart, ensaladas }) => {
+const Ensaladas = ({ ensaladita, cart, setCart, ensaladas, pedido, setPedido}) => {
 
   const { name, price, id } = ensaladita
   const addEnsalada = id => {
@@ -12,13 +15,59 @@ const Ensaladas = ({ ensaladita, cart, setCart, ensaladas }) => {
   const delEnsalada = (id) => {
     const ensaladas = cart.filter(ensaladita => ensaladita.id !== id);
     setCart(ensaladas)
-  };
+  }
+
+  console.log(cart)
+
+  const guardarPedido =  () =>{
+   
+      console.log("mensaje alerta")
+      console.log(cart)
+    const db= firebase.firestore()
+    const nuevoPedido={
+      name:cart
+    }
+
+    const data = db.collection('pedidos').add(nuevoPedido)
+
+    setCart([
+      ...cart,
+      {...nuevoPedido,id:data.id}
+
+    ])
+    setCart('')
+    }
+    
+  
+
+  
+  /*const [contador, setContador]=useState()
+    const aumentar =(id) => {
+     
+      setCart(cart.price +1)
+
+      
+  
+
+       
+    }
+  
+    
+    const restar =() => {
+      setContador(contador -1)
+
+  }
+  
+*/
+ 
+    
   return (
     <div>
       <ul>
         
 
       {ensaladas ? (
+        <div>
           <button
             className="btnDetalleMenu"
             type="button"
@@ -26,10 +75,14 @@ const Ensaladas = ({ ensaladita, cart, setCart, ensaladas }) => {
           >
             {ensaladita.name}:${ensaladita.price}
           </button>
+            
+          
+            </div>          
         ) : (
           
             <div>
              <p>{ensaladita.name}:${ensaladita.price}</p>
+           {/*  <h3>Nuestro numero aumentando :{contador} </h3>*/}
               <button
                 className=""
                 type="button"
@@ -44,7 +97,11 @@ const Ensaladas = ({ ensaladita, cart, setCart, ensaladas }) => {
               >
                 Confirmar
               </button>
-            </div>
+      {/*  <button onClick={() => aumentar(id)}>+</button>
+          <button onClick={() => restar()}>-</button>
+        */}
+            <button id="pedidococina" type="submit" className="buttonPedido" onClick={() => guardarPedido(id)}>Enviar Pedido</button>
+            </div> 
           )}
                
 
