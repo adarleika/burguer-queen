092c08a76f15150556da16e4f85395e2cc112911
marceortega <marcela.ortegao@mayor.cl>
2020-09-17 22:34:42 -0300

    cambios masterCocina para pintar pedido cocina

--- src/components/MasterCocina/MasterCocina.js
+++ src/components/MasterCocina/MasterCocina.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React from 'react'
 import './MasterCocina.css';
 
-class MasterCocina extends React.Component{
-    render () {
-        return(
-           <h1> aqui se muestran los dos componentes</h1>
-        );
-   }
-    
+import {firebase} from '../../firebase'
+
+const MasterCocina = () => {
+
+   const [pedidos,setPedidos] = React.useState([])
+
+   React.useEffect ( () =>{
+      const obtenerDatos = async () =>{
+        try{
+          const db = firebase.firestore()
+          const data = await db.collection ('pedidos').get()
+          console.log(data.docs)
+          const arrayData = data.docs.map(doc =>({id:doc.id,...doc.data()}) )
+         console.log(arrayData)
+         setPedidos(arrayData)
+   
+        } catch (error){
+          console.log(error)
+        }
+   
+   
+      }
+     obtenerDatos()
+   }, [])
+   
+
+   return (
+
+            <div>
+               <h1>Hola cocina</h1>
+         <ul>
+         {
+              pedidos.map(item =>(
+                 <li key={item.id}>{item.price}</li>
+                             ))
+
+           }
+
+           </ul> 
+      </div>
+   )
 }
-export default MasterCocina;
+
+export default MasterCocina
